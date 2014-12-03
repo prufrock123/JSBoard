187403abbd8046a35545b490f0cfb471f0603301
prufrock123 <shawnmllr86@gmail.com>
2014-12-03 17:35:10 -0600

    Ha. Well... the other approach had issues I think, anyway. But the primary problem I had was that I had a really stupid typo in my template property for my jobpostform view.

--- js/views/jobpostformView.js
+++ js/views/jobpostformView.js
@@ -5,17 +5,13 @@
 	var JobPostFormView = Backbone.View.extend({
 		tagname: "div",
 		className: "jobPostForm",
-		template: "postJobForm",
+		template: "jobPostForm",
 		render: function(){
 			var self = this;
-			var template = TemplateCache.get(this.template);
-			var html = template(this.model.toJSON());
-			this.$el.html(html);
-
-			// $.get("./templates/" + this.template + ".html", function(template){
-			// 	var html = $(template).html();
-			// 	self.$el.html(html);
-			// })
+			$.get("./templates/" + this.template + ".html").then(function(template){
+				var html = $(template).html();
+				self.$el.html(html);
+			})
 		},
 		initialize: function(){
 			
